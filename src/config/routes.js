c0782456474b19ba1a549208dbf3369f4c1d2661
Archit <archit@4atoms.se>
2021-07-06 18:01:00 +0530

    Login Page added

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,7 +3,7 @@ import { Switch, withRouter, Route } from "react-router-dom";
 import { Result } from "antd";
 
 const fetchRoutes = (containers) => {
-  const { Home } = containers;
+  const { Home, Login} = containers;
 
   return function Routes() {
     return (
@@ -11,6 +11,7 @@ const fetchRoutes = (containers) => {
         {/* Home */}
         <Route exact path="/" component={withRouter(Home)} />
         <Route exact path="/home" component={withRouter(Home)} />
+        <Route exact path="/login" component={withRouter(Login)} />
 
         {/* Router No Match - 404 */}
         <Route path="*">
