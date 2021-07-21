1a9e94563909e4c8a7d64158aac5379aa9924330
umayy25 <65256522+umayy25@users.noreply.github.com>
2021-07-21 12:56:06 +0530

    added content to right card

--- src/pages/suppliernew/supplierData.js
+++ src/pages/suppliernew/supplierData.js
@@ -1,8 +1,16 @@
 import React, { useContext, useEffect } from "react";
 import { Table, Space } from "antd";
+import { Progress } from "antd";
 import RefContext from "Utilities/refContext";
 import { useHistory } from "react-router-dom";
-import { CardRight, CardLeft, WrapperCard } from "Components/common.style";
+import {
+  CardRight,
+  CardLeft,
+  WrapperCard,
+  SpaceBar,
+  CircularBarsContainer,
+} from "Components/common.style";
+import { TitleDiv } from "../../components/common.style";
 
 const SupplierData = () => {
   const context = useContext(RefContext);
@@ -13,8 +21,47 @@ const SupplierData = () => {
   return (
     <>
       <WrapperCard>
-        <CardLeft></CardLeft>
-        <CardRight></CardRight>
+        {/* <CardLeft></CardLeft> */}
+        <CardRight>
+          <div>Supplier</div>
+          <div>CompanyId</div>
+          <
+          <CircularBarsContainer>
+            <SpaceBar />
+            <Progress
+              type="circle"
+              percent={100}
+              width={110}
+              format={() => "2  Ongoing"}
+              strokeColor={"#8FC827"}
+            />
+            <SpaceBar />
+            <Progress
+              type="circle"
+              percent={100}
+              width={110}
+              format={() => "2  To Renew"}
+              strokeColor={"#FF7A00"}
+            />
+            <SpaceBar />
+            <Progress
+              type="circle"
+              percent={100}
+              width={110}
+              format={() => "1  Upcoming"}
+              strokeColor={"#6CC1FF"}
+            />
+            <SpaceBar />
+            <Progress
+              type="circle"
+              percent={100}
+              width={110}
+              format={() => "4  Expired"}
+              strokeColor={"#DB303F"}
+            />
+            <SpaceBar />
+          </CircularBarsContainer>
+        </CardRight>
       </WrapperCard>
     </>
   );
