24eafc173ad1ca5e3df3b6feceddf54deb38711d
Archit <archit@4atoms.se>
2021-07-21 12:54:13 +0530

    Style Changed

--- src/pages/suppliernew/supplierData.js
+++ src/pages/suppliernew/supplierData.js
@@ -1,8 +1,13 @@
 import React, { useContext, useEffect } from "react";
-import { Table, Space } from "antd";
+import { Table, Space, Card, Badge, Button } from "antd";
 import RefContext from "Utilities/refContext";
 import { useHistory } from "react-router-dom";
-import { CardRight, CardLeft, WrapperCard } from "Components/common.style";
+import {
+  CardRight,
+  CardLeft,
+  WrapperCard,
+  TableWrap,
+} from "Components/common.style";
 
 const SupplierData = () => {
   const context = useContext(RefContext);
@@ -10,10 +15,77 @@ const SupplierData = () => {
     store: { suppliersList },
     actions: { getSupplierData, setId },
   } = context;
+
+  useEffect(() => {
+    getSupplierData();
+  }, []);
+
+  const columns = [
+    {
+      title: "Supplier Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Total Consultants",
+      key: "totalconsultants",
+      render: () => (
+        <Space size="middle">{suppliersList[0].consultants.length}</Space>
+      ),
+    },
+    // {
+    //   title: "Email",
+    //   dataIndex: "email",
+    //   key: "email",
+    // },
+    // {
+    //   title: "Phone Number",
+    //   dataIndex: "phone",
+    //   key: "phone",
+    // },
+    {
+      title: "Contract Status",
+      key: "contractstatus",
+      render: () => (
+        <Space size="middle">
+          <Badge count={4} className="site-badge-count-4" />
+          <Badge count={4} className="site-badge-count-4" />
+          <Badge count={4} className="site-badge-count-4" />
+        </Space>
+      ),
+    },
+    {
+      title: "View",
+      key: "view",
+      render: (supplierssList) => (
+        <Space size="middle">
+          <a onClickCapture={() => handleClick(supplierssList.id)}>View</a>
+        </Space>
+      ),
+    },
+  ];
   return (
     <>
       <WrapperCard>
-        <CardLeft></CardLeft>
+        <CardLeft>
+          <div className="site-card-border-less-wrapper">
+            <Card style={{ width: 800 }}>
+              <text>Suppliers</text>
+              <Button type="primary" shape="circle">
+                +
+              </Button>
+              {/* title="SUPPLIERS" bordered={true} */}
+              {/* <TableWrap></TableWrap> */}
+              {/* <TableTitle>SUPPLIERS</TableTitle> */}
+              <Table dataSource={suppliersList} columns={columns}></Table>
+            </Card>
+          </div>
+        </CardLeft>
         <CardRight></CardRight>
       </WrapperCard>
     </>
