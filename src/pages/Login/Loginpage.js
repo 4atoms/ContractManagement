aae95fb424390f9bf2d7644783b626b5b71d3ea6
Arun Kumar <arun1999.ak56@gmail.com>
2021-08-11 14:26:27 +0530

    login form

--- src/pages/Login/Loginpage.js
+++ src/pages/Login/Loginpage.js
@@ -1,8 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import RefContext from "Utilities/refContext";
 import "./Login.style.js";
 import { Login, LoginContainer, BTNContainer, ImageWrap } from "./Login.style";
 import logo from "Assets/images/Group35.png";
+import { Form, Input } from "antd";
 
 const Loginpage = () => {
   const Context = useContext(RefContext);
@@ -11,35 +12,48 @@ const Loginpage = () => {
     history,
   } = Context;
 
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [form] = Form.useForm();
 
-  const loginUser = () => {
-    const request = { email, password };
+  const loginUser = (formData) => {
+    const request = { email: formData.email, password: formData.password };
     login(request, history);
   };
 
   return (
     <Login>
       <LoginContainer>
-        <ImageWrap src={logo} />
-        <label>Email</label>
-        <input
-          type="text"
-          value={email}
-          autoFocus
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
+        <div style={{ margin: "10px" }}>
+          <ImageWrap src={logo} />
+        </div>
+        <Form layout="vertical" form={form} onFinish={loginUser}>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+            ]}
+          >
+            <Input allowClear placeholder="Email Id" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter password",
+              },
+            ]}
+          >
+            <Input.Password allowClear placeholder="Password" />
+          </Form.Item>
+        </Form>
         <BTNContainer>
-          <button onClick={loginUser}>Log In</button>
+          <button onClick={() => form.submit()}>Log In</button>
           {/* <p>
             Dont have an account ?
             <a href="Signup">
